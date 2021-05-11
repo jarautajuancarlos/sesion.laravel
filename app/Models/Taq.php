1d0df62d21bcc215efc96e84cabde3c9582fac14
JuanCarlos <jarauta.juancarlos@gmail.com>
2021-05-11 17:41:18 +0200

    migraciones de tablas

---
+++ app/Models/Taq.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Taq extends Model
+{
+    use HasFactory;
+}
---
+++ database/migrations/2021_05_11_153139_create_post_taq_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostTaqTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('post_taq', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            // clave foranea
+            $table->unsignedBigInteger('post_id');
+            $table->unsignedBigInteger('taq_id');
+            // restricciones de clave foranea
+            $table->foreign('post_id')->references('id')->on('posts')->onDelete('cascade');
+            $table->foreign('taq_id')->references('id')->on('taqs')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('post_taq');
+    }
+}
