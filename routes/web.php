4f8e82fc830d4575ae56858f83d73329744b72fd
JuanCarlos <jarauta.juancarlos@gmail.com>
2021-05-12 19:50:06 +0200

    creamos ruta para ver los detalles de un post

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,9 @@ use App\Http\Controllers\PostController;
 
 Route::get('/', [PostController::class, 'index'])->name('posts.index');
 
+// CREAMOS RUTA PARA VER LOS DETALLES DE UN POST
+Route::get('posts/{post}', [PostController::class, 'show'])->name('posts.show');
+
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
