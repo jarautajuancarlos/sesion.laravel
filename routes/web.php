86cce08219a9449a13cbe3d30dde71d4e9942ead
JuanCarlos <jarauta.juancarlos@gmail.com>
2021-05-12 21:14:50 +0200

    creamos ruta para filtrar posts por categorias

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,9 @@ Route::get('/', [PostController::class, 'index'])->name('posts.index');
 // CREAMOS RUTA PARA VER LOS DETALLES DE UN POST
 Route::get('posts/{post}', [PostController::class, 'show'])->name('posts.show');
 
+// CREAMOS RUTA PARA VER POSTS POR CATEGORIAS
+Route::get('category/{category}', [PostController::class, 'category'])->name('posts.category');
+
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
