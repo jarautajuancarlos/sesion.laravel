16068746590e30cdb1fa538a7a554d0e0f0807a4
JuanCarlos <jarauta.juancarlos@gmail.com>
2021-05-12 22:06:02 +0200

    cramos componente card-post y enlazamos con vista category

--- resources/views/posts/category.blade.php
+++ resources/views/posts/category.blade.php
@@ -4,33 +4,13 @@
       Categoría: {{$category->name}}
     </h1>
     @foreach($posts as $post)
-      <article class="mb-8 bg-white shadow-lg rounded-lg overflow-hidden">
-        <img
-          class="w-full h-72 object-cover object-center"
-          src="{{Storage::url($post->image->url)}}" alt="">
-          <div class="px-6 py-4">
-            <h1 class="font-bold text-xl mb-2">
-              <a href="{{route('posts.show', $post)}}">{{$post->name}}</a>
-            </h1>
-            <div class="text-gray-700 text-base">
-              {{$post->extract}}
-            </div>
-          </div>
-
-          <div class="px-6 pt-4 pb-2">
-            @foreach($post->taqs as $taq)
-              <a href=""
-                class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm text-gray-700 mr-2">
-                {{$taq->name}}
-              </a>
-            @endforeach
-
-            <div class="mt-4">
-              {{$posts->links()}}
-            </div>
-          </div>
-      </article>
+      <x-card-post :post="$post"/>
     @endforeach
+
+    <div class="mt-4">
+      {{$posts->links()}}
+    </div>
+    
   </div>
 
 
