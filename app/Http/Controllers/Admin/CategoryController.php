bc243d3b173bd0eaa5db31d7fd5de3177ac2e759
JuanCarlos <jarauta.juancarlos@gmail.com>
2021-05-13 19:08:30 +0200

    creamos vista y metodo actualizar y crear

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -47,7 +47,7 @@ class CategoryController extends Controller
 
         // NUEVO REGISTRO DE CATEGORIA
         $category= Category::create($request->all());
-        return redirect()->route('admin.categories.edit', $category);
+        return redirect()->route('admin.categories.edit', $category)->with('info', 'La categoría se creó con exito');
     }
 
     /**
@@ -81,7 +81,15 @@ class CategoryController extends Controller
      */
     public function update(Request $request, Category $category)
     {
-        //
+      // REGLAS DE VALIDACIÓN
+      $request->validate([
+        'name' => 'required',
+        'slug' => "required|unique:categories,slug,$category->id"
+      ]);
+
+      $category->update($request->all());
+
+      return redirect()->route('admin.categories.edit', $category)->with('info', 'La categoría se actualizo con exito');
     }
 
     /**
--- resources/views/admin/categories/edit.blade.php
+++ resources/views/admin/categories/edit.blade.php
@@ -3,17 +3,65 @@
 @section('title', 'Coders Free')
 
 @section('content_header')
-    <h1>Editar Categoría</h1>
+    <h1>Actualizar Categoría</h1>
 @stop
 
 @section('content')
-    <p>Welcome to this beautiful admin panel.</p>
-@stop
 
-@section('css')
-    <link rel="stylesheet" href="/css/admin_custom.css">
+@if (session('info'))
+  <div class="alert alert-success d-flex align-items-center" role="alert">
+    <strong>
+      {{session('info')}}
+    </strong>
+  </div>
+@endif
+
+<div class="card m-3">
+  <div clas="card-body">
+    {!! Form::model($category, ['route' => ['admin.categories.update', $category], 'method' => 'PUT']) !!}
+
+      <div class= "form-group m-3">
+          {{Form::label('name', 'Nombre')}}
+          {{Form::text('name', null, ['class' => 'form-control', 'placeholder' => 'Ingrese el nombre de la categoría'])}}
+
+          <!-- ERRORES DE VALIDACIÓN -->
+          @error('name')
+            <span class="text-danger">
+              {{$message}}
+            </span>
+          @enderror
+
+      </div>
+      <div class= "form-group m-3">
+          {{Form::label('slug', 'Slug')}}
+          {{Form::text('slug', null, ['class' => 'form-control', 'placeholder' => 'Ingrese el slug de la categoría', 'readonly'])}}
+
+          <!-- ERRORES DE VALIDACIÓN -->
+          @error('slug')
+            <span class="text-danger">
+              {{$message}}
+            </span>
+          @enderror
+
+      </div>
+
+      {{ Form::submit('Actualizar categoría', ['class' => 'btn btn-primary m-3 ' ]) }}
+
+    {!! Form::close() !!}
+  </div>
+</div>
 @stop
 
 @section('js')
-    <script> console.log('Hi!'); </script>
-@stop
+  <script src="{{asset('vendor/jQuery-Plugin-stringToSlug-1.3/jquery.stringToSlug.min.js')}}"></script>
+  <script>
+  $(document).ready( function() {
+  $("#name").stringToSlug({
+    setEvents: 'keyup keydown blur',
+    getPut: '#slug',
+    space: '-'
+  });
+});
+  </script>
+
+@endsection
