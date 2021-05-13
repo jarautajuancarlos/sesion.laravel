3e3c7df24d754e1a946c409b754ea2e9cd3640ab
JuanCarlos <jarauta.juancarlos@gmail.com>
2021-05-13 16:59:18 +0200

    configuramos metodo index en categoricontroller

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -17,7 +17,8 @@ class CategoryController extends Controller
      */
     public function index()
     {
-        return view('admin.categories.index');
+        $categories = Category::all();
+        return view('admin.categories.index', compact('categories'));
     }
 
     /**
@@ -49,7 +50,7 @@ class CategoryController extends Controller
      */
     public function show(Category $category)
     {
-          return view('admin.categories.show', compact('category');
+          return view('admin.categories.show', compact('category'));
     }
 
     /**
@@ -60,7 +61,7 @@ class CategoryController extends Controller
      */
     public function edit(Category $category)
     {
-          return view('admin.categories.edit', compact('category');
+          return view('admin.categories.edit', compact('category'));
     }
 
     /**
--- config/adminlte.php
+++ config/adminlte.php
@@ -242,11 +242,11 @@ return [
             'route'         => 'admin.home',
             'icon'        => 'fas fa-tachometer-alt fa-fw',
         ],
-        ['header' => 'account_settings'],
+        ['header' => 'ADMINISTRADOR'],
         [
-            'text' => 'profile',
-            'url'  => 'admin/settings',
-            'icon' => 'fas fa-fw fa-user',
+            'text' => 'Categorias',
+            'route'  => 'admin.categories.index',
+            'icon' => 'fab fa-fw fa-buffer',
         ],
         [
             'text' => 'change_password',
