03410a5bd3539f60859d626287703ed21b83502e
JuanCarlos <jarauta.juancarlos@gmail.com>
2021-05-13 16:40:47 +0200

    creamos ruta resource para los 7 metodos de crud

--- routes/admin.php
+++ routes/admin.php
@@ -5,5 +5,14 @@ use Illuminate\Support\Facades\Route;
 // IMPORTAMOS CONTROLADOR A RUTA
 use App\Http\Controllers\AdminHomeController;
 
+// IMPORTAMOS CONTROLADOR CATEGORY
+use App\Http\Controllers\Admin\CategoryController;
+
+
+
+
 // CREAMOS RUTA PARA VISTA ADMIN
 Route::get('', [AdminHomeController::class, 'index'])->name('admin.home');
+
+// CREAMOS RUTAS RESOURCE admincontroller
+Route::resource('categories', CategoryController::class)->names('admin.categories');
