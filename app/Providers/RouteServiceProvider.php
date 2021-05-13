aa7ff68e89a39e976afd904bdb77cd101018dc47
JuanCarlos <jarauta.juancarlos@gmail.com>
2021-05-13 15:41:48 +0200

    creamos archivo admin en routes y enlazamos metodo para que laravel reconozca la ruta

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -46,6 +46,11 @@ class RouteServiceProvider extends ServiceProvider
             Route::middleware('web')
                 ->namespace($this->namespace)
                 ->group(base_path('routes/web.php'));
+
+            // AÑADIMOS RUTA A ADMIN PHP
+            Route::middleware('web')
+                ->namespace($this->namespace)
+                ->group(base_path('routes/admin.php'));
         });
     }
 
