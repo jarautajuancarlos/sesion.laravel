6631b8660296785db8bf9a4f47ef699f6131f604
JuanCarlos <jarauta.juancarlos@gmail.com>
2021-05-13 20:06:10 +0200

    relleno básico funciones controller taq

--- routes/admin.php
+++ routes/admin.php
@@ -8,11 +8,17 @@ use App\Http\Controllers\AdminHomeController;
 // IMPORTAMOS CONTROLADOR CATEGORY
 use App\Http\Controllers\Admin\CategoryController;
 
+// IMPORTAMOS CONTROLADOR CATEGORY
+use App\Http\Controllers\Admin\TaqController;
+
 
 
 
 // CREAMOS RUTA PARA VISTA ADMIN
 Route::get('', [AdminHomeController::class, 'index'])->name('admin.home');
 
-// CREAMOS RUTAS RESOURCE admincontroller
+// CREAMOS RUTAS RESOURCE CATEGORIES
 Route::resource('categories', CategoryController::class)->names('admin.categories');
+
+// CREAMOS RUTAS RESOURCE CATEGORIES
+Route::resource('taqs', TaqController::class)->names('admin.taqs');
