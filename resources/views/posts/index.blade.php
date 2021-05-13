30d060746f2376993d2db19f4109835e5bbaba4a
JuanCarlos <jarauta.juancarlos@gmail.com>
2021-05-13 15:33:55 +0200

    enlazamos en la vista principal los enlaces de taq

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -13,12 +13,12 @@
             <div>
                 @foreach ($post->taqs as $taq)
                 <!-- REDIRECCIONAMOS AL METODO SHOW -->
-                  <a
-                  class="inline-block px-3 h-6 bg-{{$taq->color}}-600 text-white rounded-full">{{$taq->name}}</a>
+                  <a href="{{route('posts.taq', $taq)}}"
+                    class="inline-block px-3 h-6 bg-{{$taq->color}}-600 text-white rounded-full">{{$taq->name}}</a>
                 @endforeach
             </div>
 
-            <h1 class="text-4xl text-white leading-8 font-bold">
+            <h1 class="text-4xl text-white leading-8 font-bold mt-3">
               <a href="{{route('posts.show', $post)}}">
                 {{$post->name}}
               </a>
