5f2d98c1d72730d34118e03afe18a15e55bd0522
JuanCarlos <jarauta.juancarlos@gmail.com>
2021-05-14 15:50:39 +0200

    creamos formulario en vista para taq.create

--- resources/views/admin/taqs/create.blade.php
+++ resources/views/admin/taqs/create.blade.php
@@ -7,13 +7,54 @@
 @stop
 
 @section('content')
-    <p>Welcome to this beautiful admin panel.</p>
+    <div class="card">
+      <div class="card-body">
+        {!! Form::open(['route' => 'admin.taqs.store']) !!}
+          <div class="form-group">
+            {{Form::label('name', 'Nombre')}}
+            {{Form::text('name', null, ['class' => 'form-control',
+             'placeholder' => 'Ingrese el nombre de la etiqueta'])}}
+          </div>
+          <div class="form-group">
+            {{Form::label('slug', 'Slug')}}
+            {{Form::text('slug', null, ['class' => 'form-control',
+             'placeholder' => 'Ingrese el slug de la etiqueta', 'readonly'])}}
+          </div>
+        {!! Form::close() !!}
+
+      </div>
+    </div>
 @stop
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 @section('css')
     <link rel="stylesheet" href="/css/admin_custom.css">
 @stop
 
 @section('js')
-    <script> console.log('Hi!'); </script>
-@stop
+  <script src="{{asset('vendor/jQuery-Plugin-stringToSlug-1.3/jquery.stringToSlug.min.js')}}"></script>
+  <script>
+  $(document).ready( function() {
+  $("#name").stringToSlug({
+    setEvents: 'keyup keydown blur',
+    getPut: '#slug',
+    space: '-'
+  });
+});
+  </script>
+
+@endsection
