b922d304228391239853fe6d846b4a880167f1ab
JuanCarlos <jarauta.juancarlos@gmail.com>
2021-05-15 12:24:28 +0200

    asignacion masiva en modelo post

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -11,6 +11,8 @@ class Post extends Model
 {
     use HasFactory;
 
+    protected $guarded =['id', 'created_at', 'updated_at'];
+
     // RELACION UNO A MUCHOS INVERSA
     public function user(){
       return $this->belongsTo(User::class);
